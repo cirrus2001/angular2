b6854be629bf03b3229b7b19a8b3e400bb39a148
Alexey Dantsev <adantsev@kelsey.com>
2016-05-19 09:24:47 -0700

    change

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My 6th Angular 2 App</h1>'
+  template: '<h1>My 7th Angular 2 Appasdasdasdasdasdas</h1>'
 })
 export class AppComponent { }
